eefdaeab56d7b8a97f6a185d4cfe2a853c7781e9
Stellar Snakes <87096212+InkiiBlast225@users.noreply.github.com>
2024-11-14 16:56:50 -0800

    Update smallUpper.py

--- smallUpper.py
+++ smallUpper.py
@@ -4,7 +4,7 @@ abDict = {}
 for i in range(len(abLower)):
     abDict[abLower[i]] = abUpper[i]
 
-inTxt = input('Enter Text To Make Small Caps: ').lower() # Does tHIs work?
+inTxt = input('Enter Text To Make Small Caps: ').lower()
 outTxt = ''
 for i in inTxt:
     if abDict.get(i) != None:
